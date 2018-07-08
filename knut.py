6ebcd1f154fd77d88cb607ee4efc759bea01b44c
Stefanos Kourtis <kourtis@bu.edu>
2018-07-08 13:52:01 -0400

    Reimplemented knot utilities

--- knut.py
+++ knut.py
@@ -3,107 +3,256 @@
 #   KNOT UTILITIES                                              #
 #   ==============                                              #
 #   ( knut.py )                                                 #
-#   first instance: 20180621                                    #
-#   written by Stefanos Kourtis ( kourtis@bu.edu )              #
+#   written by Konstantinos Meichanetzidis (phykme@leeds.ac.uk) #
 #                                                               #
 #   Auxiliary routines for reading and converting between       #
-#   representations of knots and evaluation of basic knot       #
-#   properties.                                                 #
+#   representations of knots, evaluation of basic knot          #
+#   properties, encoding the Potts partition function in        #
+#   CSP form, and evaluating the Jones polynomial.              #
 #                                                               #
-#   DEPENDENCIES: numpy                                         #
+#   DEPENDENCIES: numpy, tensorcsp.py, cmath, timeit            #
 #                                                               #
 #################################################################
 
-import sys
+import sys                      # Import system-specific library
 sys.dont_write_bytecode = True  # Do not write compiled bytecode
 from numpy import *
+from tensorcsp import *
+import cmath
+from timeit import default_timer
+
 
 def writhe(x,return_signs=False):
     """ Return writhe of planar code x for len(x)>1. """
     # Since planar code contains only "under" crossings,
-    # this should yield handedness sign for each one:
+    # this yields handedness sign for each one:
     sg = array([c[1]-c[3] for c in x])
     # Make sure to treat the last arc properly:
     sg[abs(sg)>1] = -sign(sg[abs(sg)>1])
     if ( return_signs ): return sum(sg),sg
     return sum(sg)
 
-def pd2tait(x):
-    """ Return the Tait graph of planar code x.
-        Disregards handedness information. """
-    gc = pd2gc(x)
-    tg = gc2tait(gc)
-    return tg
-
-def pd2gc(X):
-    """ Convert planar data code to Gauss code.
-        Disregards handedness information. """
+def taitnumber(c):
+    """ Return Tait number from signed edge list of Tait graph. """
+    c =array(c)                 # If type(c) != ndarray
+    tau=sum(sign(c[:,0]))
+    return tau
+    
+def pd2tait(X):
+    """ Extract Tait graph from Planar Diagram presentation. """
     x  = array(X)               # If type(X) != ndarray
-    x  = x[argsort(x[:,0])]     # If X unordered
     na = x.max()                # Number of arcs
-    ai = arange(1,na+1)         # Arc indices
-    # Get crossing indexing from arc indexing.
-    # Arc indexing is redundant: make the first na/2
-    # "incoming" arcs representatives of the rest,
-    # find "representees" and put them in cp, then
-    # replace representees with representatives in ai
-    cp = (x[:,1::2]%na).min(1)  # Modulo for last arc
-    cp[cp==0] = na              # Undo modulo
-    ai[cp-1] = x[:,0]           # Replace
-    # Contiguously relabel representatives as in ix
-    _,ix = unique(ai,return_inverse=True) 
-    # Arcs not appearing as first indices in any X
-    # symbol stand for "over" crossings: give them
-    # opposite Gauss code sign, merge, and order:
-    mc = array(list(set(range(1,na+1))-set(x[:,0])))
-    aa = concatenate((x[:,0],-mc))
-    sg = sign(aa[argsort(abs(aa))])
-    gc = sg*(ix+1)
-    return gc
-
-def gc2tait(gc):
-    """ Return Tait graph from Gauss code. """
-    nc = abs(array(gc)).max() # Number of crossings
-    # Tait signs of edges are determined by "skips"
-    # w.r.t. fully alternating over/under sequence:
-    sg = (-1)**(abs(sign(gc)-array([1,-1]*nc))/2)
-    # Get crossing indices in order of appearance:
-    _,od = unique(abs(array(gc)),return_index=True)
-    ix = list(abs(array(gc))[sort(od)])
-    sg = sg[sort(od)]         # Re-sort Tait signs
-    oc = zeros(nc,int)        # Edge traversal flag
-    el = []                   # Edge list
-    nv = 0                    # Vertex counter
-    bf = 0                    # Buffer counter
-    # Gauss code of knot effectively describes an
-    # Eulerian cycle over the Tait graph with each
-    # edge doubled. Appearence of a number in the
-    # Gauss code describes traversal of an edge,
-    # sign defines which of the two edges between
-    # two vertices is traversed. We use this to
-    # reconstruct the Tait graph:
-    for i,c in enumerate(gc[:-1]):
-        c0 = abs(c)-1         # Current edge index
-        c1 = abs(gc[i+1])-1   # Next edge index
-        # If current edge has been visited before
-        # then edge has been recorded and we are
-        # now traversing "backwards", so put origin
-        # vertex in buffer:
-        if ( oc[c0] == 1 ):
-            bf = el[ix.index(c0+1)][0]
-            continue
-        # If next edge has not been visited, record
-        # "forward" traversal of current edge and
-        # put destination vertex in buffer:
-        if ( oc[c1] == 0 ):
-            el = el + [[bf,nv+1]]
-            nv = nv+1
-            bf = nv
-        # If next edge has been visited, record
-        # "forward" traversal of current edge and
-        # put origin vertex of next edge in buffer:
+    # Gather region 1
+    p0=argwhere(x==1)[0]  # Position in x where arc 1 appears first
+    p=p0 # Arc index
+    # Arc (after left turn) positions in x:
+    # replace current position with the one which belongs to a
+    # different 4-tuple (crossing), repeat until return to arc 1
+    regions=[[]]
+    # Left turn is the number to the left (up to cyclic
+    # permutation in the 4-tuple)
+    pp = argwhere(x==x[p[0],(p[1]-1)%4])
+    if not(pp[0][0]==p[0]):
+        p=pp[0]
+        regions[0].append(x[p[0],p[1]]) # Arcs belonging to region 1
+    else:
+        p=pp[1]
+        regions[0].append(x[p[0],p[1]]) # Arcs belonging to region 1
+    while not(x[p[0],p[1]]==1):
+        pp = argwhere(x==x[p[0],(p[1]-1)%4])
+        if not(pp[0][0]==p[0]):
+            p=pp[0]
+            regions[0].append(x[p[0],p[1]]) # Arcs belonging to region1
         else:
-            el = el + [[bf,el[ix.index(c1+1)][1]]]
-        oc[c0] = 1  # Record current edge traversal
-    ea = array(el)+1          # 1-based edgelist
-    return (ea.T*sg).T        # Signed edgelist
+            p=pp[1]
+            regions[0].append(x[p[0],p[1]]) # Arcs belonging to region1
+    # Gather rest of black regions.On odd (even) arc turn left (right).
+    # Skip arcs which belong to regions already.
+    for ii in range(2,na+1):
+        p1=argwhere(x==ii)  # Position in x where arc ii appears 
+        if not(p1[0][0]==p0[0]):
+            p0=p1[0]
+        else:
+            p0=p1[1]
+        p=p0
+        # Only continue if the arc is unaccounted for:
+        if sum([ii in r for r in regions])==0:
+            # Left turns:
+            if ii%2==1:
+                regions.append([])
+                # Left turn is the number to the left (up to cyclic
+                # permutation in the 4-tuple)
+                pp = argwhere(x==x[p[0],(p[1]-1)%4])
+                if not(pp[0][0]==p[0]):
+                    p=pp[0]
+                    regions[len(regions)-1].append(x[p[0],p[1]])
+                else:
+                    p=pp[1]
+                    regions[len(regions)-1].append(x[p[0],p[1]])
+                while not(x[p[0],p[1]]==ii):
+                    pp = argwhere(x==x[p[0],(p[1]-1)%4])
+                    if not(pp[0][0]==p[0]):
+                        p=pp[0]
+                        regions[len(regions)-1].append(x[p[0],p[1]])
+                    else:
+                        p=pp[1]
+                        regions[len(regions)-1].append(x[p[0],p[1]])
+            # Right turns:
+            if ii%2==0:
+                regions.append([])
+                # Right turn is the number to the right (up to cyclic
+                # permutation in the 4-tuple)
+                pp = argwhere(x==x[p[0],(p[1]+1)%4])
+                if not(pp[0][0]==p[0]):
+                    p=pp[0]
+                    regions[len(regions)-1].append(x[p[0],p[1]])
+                else:
+                    p=pp[1]
+                    regions[len(regions)-1].append(x[p[0],p[1]])
+                while not(x[p[0],p[1]]==ii):
+                    pp = argwhere(x==x[p[0],(p[1]+1)%4])
+                    if not(pp[0][0]==p[0]):
+                        p=pp[0]
+                        regions[len(regions)-1].append(x[p[0],p[1]])
+                    else:
+                        p=pp[1]
+                        regions[len(regions)-1].append(x[p[0],p[1]])
+    # Now that we have all the regions in terms of the arcs that
+    # enclose the region we go through all crossings (4 tuples)
+    # and obtain the signed edge list.
+    el=[[] for ii in range(len(x))]
+    for iic,ii in enumerate(x):
+        if not(na in ii and 1 in ii):
+            eps= (-1)**min(ii) *(-1)**(argmin(ii)+1)
+        else:
+            eps= (-1)**max(ii) *(-1)**(argmax(ii)+1)
+        pair1=[ii[0],ii[1]]
+        pair2=[ii[2],ii[3]]
+        for rc,r in enumerate(regions):
+            if (pair1[0] in r) and (pair1[1] in r):
+                el[iic].append((rc+1)*eps)
+    
+            if (pair2[0] in r) and (pair2[1] in r):
+                el[iic].append((rc+1)*eps)
+        pair1=[ii[0],ii[3]]
+        pair2=[ii[1],ii[2]]   
+        for rc,r in enumerate(regions):
+            if (pair1[0] in r) and (pair1[1] in r):
+                el[iic].append((rc+1)*eps)
+            if (pair2[0] in r) and (pair2[1] in r):
+                el[iic].append((rc+1)*eps)
+    return array(el)
+
+def tpotts(q):
+    """ Relation between Jones variable t (complex for 0<q<4)
+        and number of spin states q in the Potts model. """
+    t=.5*(q+sqrt(q)*cmath.sqrt(q-4)-2)
+    return t
+
+def boltz_tensor(ek,q):
+    """ Boltzmann factor matrix for interaction bond of q-state
+        Potts model for the Jones polynomial. Diagonal terms are
+        set to ek (in general complex). Off-diagonal terms are set
+        to 1. Some diagonal entries are later turned to ek**(-1)
+        according to the Tait sign of the corresponding edge in
+        the Tait graph. This is done with boltz_entry() when the
+        tensor network is built. """
+    b = ones([q,q],dtype=complex)
+    b[diag_indices(q)] = ek*ones(q)
+    return b
+
+def boltz_entry(b,q,i,m):
+    """ Enforce Tait sign convention in Boltzmann tensor. """
+    return b[unravel_index(i,shape(b))]**((-1)**(m+1))
+
+def boltz_tngraph(c,ek,q,dtype=complex):
+    """ Constructs graph object endowed with variable and
+        interaction tensors corresponding to q-state Potts model
+        on graph dictated by signed edge list c. A positive sign
+        in an entry of c means a positive Tait sign, and in turn
+        a ek bond. Negative sign is a ek**(-1) bond. """
+    b  = boltz_tensor(ek,q)
+    gt = lambda i,m: boltz_entry(b,q,i,m)
+    g  = cnf_tngraph(c,q,gate=gt,dtype=dtype)
+    return g
+
+def DeltaH_greedy(c):
+    """ Returns maximal degree encountered during greedy
+        contraction of the Tait graph encoded in edgelist c. """
+    nc = len(c)  # number of crossings
+    q=1
+    if nc>0:
+        nv=cnf_nvar(c)       
+        ekpotts=-tpotts(q)  # Potts interaction for Jones
+        g=boltz_tngraph(c,ekpotts,q)
+        b,gn = contract_greedy(g,combine_attrs=None)
+        maxdeg=max(b)
+    else:
+        nv=1
+        maxdeg=1
+    
+    return maxdeg
+
+def DeltaH_METIS(c):
+    """ Returns maximal degree encountered during METIS
+        contraction of the Tait graph encoded in edgelist c. """
+    nc = len(c)  # number of crossings
+    q=1
+    if nc>0:
+        nv=cnf_nvar(c)       
+        ekpotts=-tpotts(q)  # Potts interaction for Jones
+        g=boltz_tngraph(c,ekpotts,q)
+        m = recursive_bipartition(g)
+        md,sg = contract_dendrogram(g,m,combine_attrs=None)
+        maxdeg=max(md)
+    else:
+        nv=1
+        maxdeg=1
+    
+    return maxdeg
+
+def Jones_greedy(c,tau,w,q):
+    """ Returns Jones polynomial evaluated at t(q) via greedy
+        contraction of the tensor network of the knot encoded
+        in edgelist c. w is the writhe of the knot. """
+    nc = len(c)  # number of crossings
+    if nc>0:
+        nv=cnf_nvar(c)       
+        ekpotts=-tpotts(q)
+        g=boltz_tngraph(c,ekpotts,q)
+        t1=default_timer()
+        b,gn = contract_greedy(g,combine_attrs=dict(attr=attr_contract))
+        t2=default_timer()
+        runtime=t2-t1
+        Z=gn.vs["attr"][0][1]
+    else:
+        nv=1
+        Z=q
+        runtime=0
+    # Multiply Z with appropriate prefactors to get Jones polynomial:
+    jpoly =  Z * (-tpotts(q)**.5-tpotts(q)**-.5)**(-nv-1) * (-tpotts(q)**(3./4))**w * tpotts(q)**(1./4*tau)
+    return jpoly,runtime
+
+def Jones_METIS(c,tau,w,q):
+    """ Returns Jones polynomial evaluated at t(q) via METIS
+        contraction of the tensor network of the knot encoded
+        in edgelist c. w is the writhe of the knot. """
+    nc = len(c)  # number of crossings
+    if nc>0:
+        nv=cnf_nvar(c)       
+        ekpotts=-tpotts(q)
+        g=boltz_tngraph(c,ekpotts,q)
+        t1=default_timer()
+        m = recursive_bipartition(g)    # Uses METIS
+        md,sg = contract_dendrogram(g,m,combine_attrs=dict(attr=attr_contract))
+        t2=default_timer()
+        runtime=t2-t1
+        Z=sg.vs["attr"][0][1]
+    else:
+        nv=1
+        Z=q
+        runtime=0
+    # Multiply Z with appropriate prefactors to get Jones polynomial:
+    jpoly =  Z * (-tpotts(q)**.5-tpotts(q)**-.5)**(-nv-1) * (-tpotts(q)**(3./4))**w * tpotts(q)**(1./4*tau)
+    return jpoly,runtime
+
